ebe4e7c9592affbdfd7b36e2c25b536addf58773
Jeremy Parker <jrobinparker@gmail.com>
2018-06-18 20:26:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessor :name, :type
+  attr_reader :name, :type
+  
+  def initialize(name, type, db)
+    @db = db
+    @name = name
+    @type = type
+  end
+  
+  def self.save 
+    
+  
 end
